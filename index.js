72541657493f39d04f718756aac8c3d958b6640f
Sammy-CK <sammycherono2696@gmail.com>
2022-11-19 11:45:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    "name"          : "Sammy",
+    "streetAddress" : "Embakasi"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const tempO = {...employee};
+    tempO[key] = value;
+    return tempO;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const tempO = {...employee};
+    delete tempO[key];
+    return tempO;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
